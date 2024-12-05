ba7951827509fbfd79792d7d7498942070325a06
Niamh Driscoll <niamhmolson@outlook.com>
2024-12-05 16:28:06 +0000

    Fixed Favicon

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, redirect, url_for
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return render_template("index.html")
+@app.route("/about")
+def about():
     return render_template("index.html")
 
 if __name__ == '__main__':
